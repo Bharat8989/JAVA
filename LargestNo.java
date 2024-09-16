f259aed0913299f1629642b4b8dae9bed08f9d29
Bharat <kadamb208@gmail.com>
2024-09-16 13:56:20 +0530

    leap year add progream

---
+++ LargestNo.java
@@ -0,0 +1,21 @@
+// write a program to find the largest number Between two give number
+
+import java.util.Scanner;
+public class LargestNo{
+    public static void main(String args[])
+    {
+        // int a;
+        Scanner S=new Scanner(System.in);
+        System.out.println("value one enter:");
+        float num1=S.nextInt();
+        System.out.println("value two enter:");
+        float num2=S.nextInt();
+        if(num1>num2){
+            System.out.println("num1 is big");
+        }
+        else{
+            System.out.println("num 2 is big");
+        }
+
+    }
+}
---
+++ LeapYear.java
@@ -0,0 +1,19 @@
+// Leap Year Checker
+// Write a program to check whether a year is a leap year or not.
+
+// A year is a leap year if it is divisible by 4 but not divisible by 100, except if it is divisible by 400.
+
+import java.util.Scanner;
+class LeapYear{
+    public static void main(String[] args) {
+        Scanner S=new Scanner(System.in);
+        System.out.println("enter any year:");
+        int year=S.nextInt();
+        if(year%4==0 && year%100 !=0 || year%400==0){
+            System.out.println("this "+year +" is leap");
+        }
+        else{
+            System.out.println("this "+year +" is not leap year");
+        }
+    }
+}
