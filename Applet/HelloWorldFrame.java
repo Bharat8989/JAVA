21181383f84949d67d0971b3bb206cfeda4d0040
Bharat <kadamb208@gmail.com>
2024-10-09 18:41:55 +0530

    adding files for Applet

--- Applet/HelloWorldFrame.java
+++ Applet/HelloWorldFrame.java
@@ -1,18 +1,49 @@
+// using the jFrame 'hello world'
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-public class HelloWorldFrame extends JFrame {
-    public HelloWorldFrame() {
-        // setTitle("Hello World Frame");
-        add(new JLabel("Hello World"));
-        setSize(300, 100);
+public class HelloWorldFrame extends JFrame{
+    public HelloWorldFrame(){
+        add(new JLabel("hello world"));
+        setSize(700,300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-    }   
-    //what 
 
-    public static void main(String[] args) {
-        new HelloWorldFrame(); 
+    }
+    public static void main(String args[])
+    {
+        new HelloWorldFrame();
     }
 }
- 
+
+// import javax.swing.JFrame;
+// import javax.swing.JLabel;
+
+// public class HelloWorldFrame extends JFrame {
+//     public HelloWorldFrame() {
+//         // setTitle("Hello World Frame");
+//         add(new JLabel("Hello World"));
+//         setSize(300, 100);
+//         setDefaultCloseOperation(EXIT_ON_CLOSE);
+//         setVisible(true);
+//     }   
+//     //what 
+
+//     public static void main(String[] args) {
+//         new HelloWorldFrame(); 
+//     }
+// }
+//  import javax.swing.JFrame;
+//  import javax.swing.JLabel;
+//  class HelloWorldFrame extends JFrame{
+//     public HelloWorldFrame(){
+//         add(new JLabel("hello world"));
+//         setSize(300,100);
+//         setDefaultCloseOperation(EXIT_ON_CLOSE);
+//         setVisible(true);
+
+//     }
+//     public static void main(String args [])
+//     {
+//         new HelloWorldFrame();
+//     }
+//  }
