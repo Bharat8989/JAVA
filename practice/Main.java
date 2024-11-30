5629b0b6a06b5d0cc0c4a4e8fd294ec05f3bb1dc
Bharat <kadamb208@gmail.com>
2024-11-30 13:23:59 +0530

    single inheritance method run

--- practice/Main.java
+++ practice/Main.java
@@ -1,22 +1,31 @@
-// oops stand for object oriented programming language 
-// i)class and object 
-// class is blueprint or template
-class Main
-{
-    String name ="bharat";
-    int age =20;
+//ii)Inheritance 
+// inheritance there are five types of inheritance 
+// single multilevel Hierarchical
+//single inheritance it single it inheritance one class to another class 
+//using the  extends keyword
+//examples
+class A {
+    void show()
+    {
+        System.out.println("show method run");
 
-    static void show(){
-        System.out.println("static method without call the run ");
     }
 
-    public static void main (String args[])
+}
+class B extends A{
+    void showB()
     {
-        System.out.println("Main method is call");
-        Main k=new Main();
-        System.out.println("your name is :"+k.name);
-        System.out.println("your age is:"+k.age);
-        show(); // not using the main method
-        
+        System.out.println("B method is run ");
+
+    }
+
+}
+class Main {
+    public static void main(String args[])
+    {
+        B kk=new B();
+        kk.show();
+        kk.showB();
+        System.out.println("both method are single inheritance");
     }
 }
