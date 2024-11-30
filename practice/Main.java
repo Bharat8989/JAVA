a99bb11c08be6a123408b400be7a1cb0f2f8ee5a
Bharat <kadamb208@gmail.com>
2024-11-30 11:39:46 +0530

    conditional statement

--- practice/Main.java
+++ practice/Main.java
@@ -1,38 +1,26 @@
-// control statement 
-//there are two types of control statement i)looping statement ii)conditional statement 
-// i)for loop statement 
-// ii)while loop 
-// examples
-class Main{
-    void run(){
-        for(int i=0;i<=10;i++){
-            System.out.println(i);
-        }
-        
-    }
-    void whileLoop()
-    {
-        int j=1;
-        while(j<=6){
-        System.out.println(j);
-        j++;
-    }
-    void doWhileLoop(){
-        int k=20;
-        do{
-           
-            System.out.println(k);
-            k++;
-        }
-        while(k <=30);
-    }
-    }
-    public static void main(String args[])
+// conditional statement 
+// if if else if else if and last is the nested if 
+
+class Main {
+    void ifElse(){
+        int age=18;
+        if(age<=8)
         {
-            Main k=new Main();
-            k.run();
-            k.whileLoop();
-            k.dowhileLoop();
-            System.out.println("hello Main class ");
+            System.out.println("you are the  voting  ");
         }
+        else{
+            System.out.println("your are not voting ");
+        }
+    }
+    public static void main (String args[])
+    {
+        Main ifelse=new Main();
+        ifelse.ifElse();
+        int a=20;
+        int b=20;
+        if(a==b)
+        {
+            System.out.println("a and b both are equal");
+        }
+    }
 }
