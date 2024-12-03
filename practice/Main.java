e0a9f5dcf05829654e57e393a311c1491482e52f
Bharat <kadamb208@gmail.com>
2024-12-03 17:53:46 +0530

    loopint statement

--- practice/Main.java
+++ practice/Main.java
@@ -4,98 +4,64 @@
 // Polymorphism
 // Abstraction
 // Encapsulation
-/* inheritance there are main 3 types of i single ii)multiLever iii 
- * Single level
-Multi-level
-Hierarchical
-Multiple
-Hybrid
-multiple and hybrid are both not using the in java 
-it use to 
-  */
-//   single multiLever hierarchical
-//   class A{
-//     public void showA()
-//     {
-//         System.out.println("A method run");
-//     }
-//   }
-//   class B extends A{
-//     public void showB()
-//     {
-//         System.out.println("B method run");
-//     }
-//   }
-//   class Main extends A{
-//     public static void main(String[] args) {
-//         Main kk=new Main();
-//         B kkkk=new B();
-//         kkkk.showB();
-//         kk.showA();
-//         // kk.showB();
-//     }
-//   }
-// multiple 
-// interface Animal{
-//     void showA();
-   
-// }
-// interface Dog{
-//     void showB();
-// }
-// class Main implements Animal,Dog{
-//     public void showA(){
-//         System.out.println("show a method ");
 
+// looping statement 
+/* there are varies types of looping statement i for loops ii while loops iii do while loops last is nested loops  */
+// public class Main {
+//   public static void main (String args[])
+//     {
+//         for(int i=0;i<=20;i++)
+//     {
+//         System.out.println(i);
 //     }
-//     public void showB(){
-//         System.out.println("show b method");
-//     }
-//     public static void main(String args[]){
-//     Main kk=new Main();
-//     kk.showA();
-//     kk.showB();
 //     }
 // }
 
+//while loop 
+// public class Main {
 
-// Polymorphism
-// there are two types of polymorphism one is the compile time polymorphism second is the runtime polymorphism
-// examples
-// same name but different parameter
-// class Main {
-//     int add(int a ,int b)
-//     {
-//         return a+b;
-//     }
-//     int add(int a, int b ,int c)
-//     {
-//         return a+b+c;
-//     }
 //     public static void main(String args[])
 //     {
-//         Main kk=new Main();
-//         kk.add(23, 27);
-//         kk.add(10, 20, 30);
-//         System.out.println("add of two number:"+kk.add());
-//         System.out.println("add of three number:"+kk.add());
+//         int i=10;
+//         while (i<=20) {
+//             System.out.println(i);
+//             i++;
+            
+//         }
 //     }
 // }
-// abstract
-// abstract class Animal{
-//     public abstract void showA();
-// }
-// class Main extends Animal{
-//     public void showA(){
-//         System.out.println("show a method for abstract method ");
 
-//     }
-//     public static void main (String args[])
+
+// do while loop 
+// public class Main {
+
+//     public static void main(String args[])
 //     {
-//         Main kk=new Main();
-//         kk.showA();
+//         int i=2;
+//         do
+//         {
+//             System.out.println(i);
+//             i++;
+//         }
+//         while(i<=30);
 //     }
 // }
+
+
+//nested loop 
+public class Main {
+
+    public static void main(String args[])
+    {
+        for(int i=2;i<=3;i++)
+        for(int j=2;j<=4;j++)
+        {
+            System.out.println(i+" * "+j+" = "+i*j);
+        }
+        System.out.println();
+    }
+}
+
 
 // lang packages
 // class Main {
@@ -116,21 +82,21 @@ it use to
 //         System.out.println("your name is:"+name);
 //     }
 // }
-import java.awt.*;
+// import java.awt.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Frame frame = new Frame("AWT Example");
-        Button button = new Button("Click Me!");
+// public class Main {
+//     public static void main(String[] args) {
+//         Frame frame = new Frame("AWT Example");
+//         Button button = new Button("Click Me!");
 
-        button.setBounds(50, 100, 80, 30); // Set button position
-        frame.add(button); // Add button to frame
+//         button.setBounds(50, 100, 80, 30); // Set button position
+//         frame.add(button); // Add button to frame
 
-        frame.setSize(300, 200); // Set frame size
-        frame.setLayout(null);   // Disable default layout
-        frame.setVisible(true);  // Make frame visible
-    }
-}
+//         frame.setSize(300, 200); // Set frame size
+//         frame.setLayout(null);   // Disable default layout
+//         frame.setVisible(true);  // Make frame visible
+//     }
+// }
 
 
 // import java.io.*;
