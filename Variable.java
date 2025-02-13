c2512f0d980efff2d20dc6d6895f38e6045dbafc
Bharat <kadamb208@gmail.com>
2025-02-13 18:54:45 +0530

    arithmetic operators

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+   
+   //arithmetic operators 
+   public static void main(String args[]){
+    int a=20;
+   int b=3;
+   int c=23;
+   System.out.println(a+b);
+   System.out.println(a-b);
+   System.out.println("a/b:" + a/b);
+   System.out.println("a%b:" + a%b);
+   System.out.println("a*b:" + a*b);
+   System.out.println(c);
+
+
+
+
+}
+}
