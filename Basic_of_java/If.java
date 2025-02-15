69ca909e211f19de6b1ef63aa07855b4decdc0b6
Bharat <kadamb208@gmail.com>
2025-02-15 08:59:24 +0530

    if else problems solving

---
+++ Basic_of_java/If.java
@@ -0,0 +1,18 @@
+package Basic_of_java;
+
+public class If {
+    public static void main(String args[]){
+ 
+        int a=23;
+        if(a%2==0){
+            System.out.println("It is even number" +a);
+
+        }
+        else{
+            System.out.println("It is odd number "+a);
+        }
+        
+
+    }
+    
+}
--- Basic_of_java/Main.java
+++ Basic_of_java/Main.java
@@ -1,3 +1,4 @@
+package Basic_of_java;
 import java.util.Scanner;
 
 public class Main {
