5ef7fe26a7d2e048e86ea0ce56287c2a50f64b02
Bharat <kadamb208@gmail.com>
2025-02-16 15:06:18 +0530

    star pattern

--- Basic_of_java/Loop.java
+++ Basic_of_java/Loop.java
@@ -1,33 +1,21 @@
 package Basic_of_java;
+// import java.util.Scanner;
+public class Loop {
 
-import java.util.Scanner;
-
-//while lop 
-//do while loop
-//for loop
-class Loop {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-    System.out.println("enter any natural number:");
-    int n=sc.nextInt();
-    int reversed=0;
-    int original=n;
-
-    while(n>0){
-        reversed=reversed*10+n%10;
-        n=n/10;
-
-
+    public static void main(String args[])
+    {
+ 
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println(); {
+            
+                
+            }
+        }
+        
     }
-    if(original==reversed){
-        System.out.println(original+" is a palindrome");
-    }
-    else{
-        System.out.println(original+" is not a palindrome");
-    }
-
-   
     
-}
-
 }
