2fda0250800a27cce8ab59ffa73ecaf58256df0c
Bharat <kadamb208@gmail.com>
2025-02-18 21:15:40 +0530

    some change

---
+++ Basic_of_java/Main1.java
@@ -0,0 +1,16 @@
+package Basic_of_java;
+
+public class Main1 {
+ 
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c=30;
+        
+
+         c=b-a;
+         System.out.println(c);
+
+    }
+}
