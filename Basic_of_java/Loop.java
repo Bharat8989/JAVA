59613e5bd66168be6399fa63b46a3850f97e1058
Bharat <kadamb208@gmail.com>
2025-02-19 11:22:03 +0530

    add main files

--- Basic_of_java/Loop.java
+++ Basic_of_java/Loop.java
@@ -5,8 +5,8 @@ public class Loop {
     public static void main(String args[])
     {
  
-        for(int i=1;i<=4;i++ )
-        {
+        for(int i=1;i<=5;i++ )
+        { 
             for(int j=1;j<i+1;j++){
                 System.out.print(j);
             }
