f6b2130236f39bb8cd6faec6da1b421b8070c689
Bharat <kadamb208@gmail.com>
2025-02-27 17:56:06 +0530

    star pattern

--- Basic_of_java/Main1.java
+++ Basic_of_java/Main1.java
@@ -1,16 +1,18 @@
 package Basic_of_java;
 
-public class Main1 {
- 
-    public static void main(String args[])
-    {
-        int a=10;
-        int b=20;
-        int c=30;
+import java.util.Scanner;
+
+class Main1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *"); // Print on the same line
+            }
+            System.out.println(); // Move to the next line after each row
+        }
 
-         c=b-a;
-         System.out.println(c);
-
+        sc.close(); // Close the scanner to avoid resource leak
     }
 }
