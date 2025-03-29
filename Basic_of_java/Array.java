727d27184d05ad13e6bb50fbd01a9500d623a61f
Bharat <kadamb208@gmail.com>
2025-03-29 09:39:50 +0530

    array

--- Basic_of_java/Array.java
+++ Basic_of_java/Array.java
@@ -1,14 +1,15 @@
 package Basic_of_java;
+
 class Array {
-   
-    // a simple array where elements are stored in a single row 
-    public static void main(String args[]){
-    int array[]={10,20,30,40};
-    
-    System.out.println(array.length);
-    for(int i=0;i<array.length;i++)
-    {
-        System.out.print(i+ " ");
+
+    // a simple array where elements are stored in a single row
+    public static void main(String args[]) {
+        // revers array
+        int arr[][] = new int[3][];
+        arr[0] = new int[2]; // First row with 2 elements
+        arr[1] = new int[3]; // Second row with 3 elements
+        arr[2] = new int[1]; // Third row with 1 element
+
     }
-    }
+
 }
