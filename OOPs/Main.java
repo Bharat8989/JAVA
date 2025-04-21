7d409006cf49697b5c9f2a897e4e9c4257fee90d
Bharat <kadamb208@gmail.com>
2025-04-21 19:31:53 +0530

    difference between class and object

--- OOPs/Main.java
+++ OOPs/Main.java
@@ -1,14 +1,22 @@
+// Class definition (blueprint)
 class Car {
-    void Show()
-    {
-        System.out.println("Show method call");
-    }
+    String brand;
+    int year;
 }
-class Main {
-    public static void main(String args[])
-    {
- 
-        Car car=new Car();
-        car.Show();
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating objects from class
+        Car car1 = new Car();  // Object 1
+        car1.brand = "Toyota";
+        car1.year = 2020;
+
+        Car car2 = new Car();  // Object 2
+        car2.brand = "Honda";
+        car2.year = 2022;
+
+        // Accessing data through objects
+        System.out.println("Car 1: " + car1.brand + " " + car1.year);
+        System.out.println("Car 2: " + car2.brand + " " + car2.year);
     }
 }
