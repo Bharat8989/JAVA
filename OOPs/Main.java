5db6b007e5d6c227598eccdd2e038cc8ea890e26
Bharat <kadamb208@gmail.com>
2025-04-21 18:19:44 +0530

    object and class examples

---
+++ OOPs/Main.java
@@ -0,0 +1,14 @@
+class Car {
+    void Show()
+    {
+        System.out.println("Show method call");
+    }
+}
+class Main {
+    public static void main(String args[])
+    {
+ 
+        Car car=new Car();
+        car.Show();
+    }
+}
