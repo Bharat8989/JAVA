c1ecd5f58056deb551d581c0dd539eec4ad000ba
Bharat <kadamb208@gmail.com>
2025-04-22 21:55:17 +0530

    star patter

--- OOPs/Main.java
+++ OOPs/Main.java
@@ -1,26 +1,15 @@
-//write a program to check if a number is prime using a for loop
-class Main
-{
-    public static void main(String[] args) 
+class Main {
+    public static void main(String args[])
     {
-
-        int num=13;
-        boolean isPrime=true;
-        if(num<=1){
-            isPrime=false;
-        }else{
-            for(int i=2;i<num;i++){
-                if(num % i==0){
-                    isPrime=false;
-                    break;
-                }
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
             }
-        }
-        if(isPrime){
-            System.out.println(num + " is prime number");
-        }
-        else{
-            System.out.println(num + " this is not prime number");
+           for(int k=1;k<=1*i-1;k++){
+            System.out.print("*");
+           }
+           System.out.println();
         }
     }
 }
