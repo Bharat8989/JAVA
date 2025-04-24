8f83621d297497b5baecc322e3f5612ead4e1290
Bharat <kadamb208@gmail.com>
2025-04-24 14:32:12 +0530

    constructor

---
+++ Constructor/Main.java
@@ -0,0 +1,45 @@
+ /* Constructor
+ * Constructor name =class name
+ * it has no return type 
+ * it runs automatically when the object is create.
+ * 1 default Constructor
+ * a Constructor with no parameters
+ * java provides it automatically if you don't define any Constructor
+ * 
+ * 2  Parameterized Constructor
+ * a constructor that accepts arguments to initialize object variables
+ */
+// class Student{
+//     Student(){
+//         System.out.println("default Constructor");
+//     }
+// }
+// class Main {
+//     public static void main(String args[])
+//     {
+//         Student s=new Student();
+//     }
+// }
+class Student 
+{
+    String name;
+        int roll;
+    Student(String n,int a)
+    {
+        name=n;
+        roll=a;
+
+    }
+    void display(){
+        System.out.println("name:"+name);
+        System.out.println("roll:"+roll);
+    }
+
+}
+class Main{
+    public static void main(String args[])
+    {
+        Student s=new Student("bharat",34);
+        s.display();
+    }
+}
