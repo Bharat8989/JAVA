ebd6ebf39352fd842718b96d635eb46e5058ce6b
Bharat <kadamb208@gmail.com>
2025-04-24 16:24:05 +0530

    thread

---
+++ Constructor/ThreadExample.java
@@ -0,0 +1,14 @@
+class MyThread extends Thread{
+    public void run(){
+        System.out.println("thread running ");
+    }
+}
+class ThreadExample{
+     public static void main(String args[])
+    {
+        MyThread t=new MyThread();
+        // t.Show();
+        t.start();
+    }
+    
+}
