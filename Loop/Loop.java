be8a0fbdb3db213ac0befd19f99286f60fdc5631
Bharat <kadamb208@gmail.com>
2025-04-25 18:59:52 +0530

    loop for star patter

---
+++ Loop/Loop.java
@@ -0,0 +1,13 @@
+class Loop{
+    public static void main(String args[]){
+      //star patter questions 
+     for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(i+"  ");
+        }
+        System.out.println(" ");
+     }
+      
+       
+    }
+}
