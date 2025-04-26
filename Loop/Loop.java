7b145edd8b30cebc558c2d37acce560423beee77
Bharat <kadamb208@gmail.com>
2025-04-26 13:43:54 +0530

    leap year

--- Loop/Loop.java
+++ Loop/Loop.java
@@ -1,13 +1,25 @@
-class Loop{
-    public static void main(String args[]){
-      //star patter questions 
-     for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            System.out.print(i+"  ");
-        }
-        System.out.println(" ");
-     }
+// class Loop{
+//     public static void main(String args[]){
+//       //star patter questions 
+//      for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             System.out.print(i+"  ");
+//         }
+//         System.out.println(" ");
+//      }
       
        
+//     }
+// }
+class Loop{
+    public static void main(String args[])
+    {
+        int year=2021;
+        if((year%4==0 && year%100 !=0) || year % 400==0){
+            System.out.println(year+" this is leap year");
+        }
+        else{
+            System.out.println(year+" this is not leap year");
+        }
     }
 }
