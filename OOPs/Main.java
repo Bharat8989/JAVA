a40bf632c2a913463e2d19f3ac28398b449428c7
Bharat <kadamb208@gmail.com>
2025-05-26 19:07:47 +0530

    oops

--- OOPs/Main.java
+++ OOPs/Main.java
@@ -1,33 +1,17 @@
-/* 1 Inheritance
- * 2 Multi-level Inheritance
- * 3 Hierarchical Inheritance
- * 4 Hybrid inheritances
- * 5 multiple inheritances
- * 
- */
-interface A{
-    void showA();
-
-} 
-interface B
-{
-    void showB();
-}
-class C implements A, B{
-    public void showA(){
-        System.out.println("show A method run ");
-    }
-   public void showB(){
-        System.out.println("show B method run ");
+//class and object 
+//real world entity 
+class Student {
+     public void show()
+    {
+        System.out.println("show a method ");
     }
 }
 class Main {
+   
     public static void main (String args[])
     {
-        C b=new C();
-        b.showA();
-       
-        b.showB();
+       Student s=new Student();
+       s.show();
+
     }
 }
-
--- Thread/Main.java
+++ Thread/Main.java
@@ -1,14 +1,19 @@
+/* star patter 
+ *   *
+ *   * *
+ *   * * * 
+ *   * * * *
+ */
 class Main {
     public static void main(String args[]){
         int rows=4;
         for(int i=1;i<=rows;i++){
-            for(int space=1;space<=rows-i;space++){
-                System.out.print(" ");
-            }
-            for(int star=1;star<=i;star++){
-                System.out.print(star+" ");
+            for(int j=1;j<=rows;j++){
+                System.out.print("* ");
             }
             System.out.println();
+            
         }
+
     }
 }
