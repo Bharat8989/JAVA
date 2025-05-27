ff30cc2188e2d8b2bd76b37b45658c1cf93aea9d
Bharat <kadamb208@gmail.com>
2025-05-27 15:44:39 +0530

    oops topices

--- OOPs/Main.java
+++ OOPs/Main.java
@@ -1,17 +1,22 @@
-//class and object 
-//real world entity 
-class Student {
-     public void show()
-    {
-        System.out.println("show a method ");
+//single inheritance 
+//one class inheritance to another class
+class A{
+    public void showA(){
+        System.out.println("show a method call");
+    }
+}
+class B extends A{
+    public void showB(){
+        System.out.println("show b method call");
     }
 }
 class Main {
-   
-    public static void main (String args[])
-    {
-       Student s=new Student();
-       s.show();
+    public static void main (String args[]){
+        B b=new B();
+        b.showA();
+        b.showB();
 
+        A a=new A();
+        a.showA();
     }
 }
