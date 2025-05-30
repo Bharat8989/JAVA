3d7aee82f91e2a8ee1ba60f0ea6ec8983f59551a
Bharat <kadamb208@gmail.com>
2025-05-30 16:12:43 +0530

    Object-Oriented Programming

--- OOPs/Main.java
+++ OOPs/Main.java
@@ -1,22 +1,37 @@
-//single inheritance 
-//one class inheritance to another class
-class A{
-    public void showA(){
-        System.out.println("show a method call");
-    }
-}
-class B extends A{
-    public void showB(){
-        System.out.println("show b method call");
-    }
-}
-class Main {
-    public static void main (String args[]){
-        B b=new B();
-        b.showA();
-        b.showB();
+//types of inheritance in java 
+//one class inheritance to another class 
+//single one child class inheritance form one parent class 
+//multilevel a class inherits form a class which itself is inherited 
+// hierarchical  inheritance multiple class inherits form a single parent class 
 
-        A a=new A();
-        a.showA();
+//multiple inheritance (via interface ) a class implements multiple interface 
+//hybrid inheritance :combination of two or more types multiple and single inheritance
+
+interface A{
+    void showA();
+
+}
+interface B{
+    void showB();
+}
+class C implements A,B{
+    public void showA(){
+        System.out.println("show a method ");
+    }
+    public void showB(){
+        System.out.println("show b method");
+    }
+}
+class D extends C{
+    void showD(){
+        System.out.println("show d Method ");
+    }
+}
+public class Main {
+    public static void main (String args[]){
+        D obj=new D();
+        obj.showA();
+        obj.showB();
+        obj.showD();
     }
 }
