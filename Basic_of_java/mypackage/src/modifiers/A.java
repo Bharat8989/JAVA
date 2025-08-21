17d8f3333a34eaf741b810c558b9ab9c7da06cf0
Bharat <kadamb208@gmail.com>
2025-08-21 20:01:24 +0530

    access modifiers

---
+++ Basic_of_java/mypackage/src/modifiers/A.java
@@ -0,0 +1,6 @@
+package modifiers;
+
+public class A {
+    public int x=10;
+}
+
---
+++ Basic_of_java/mypackage/src/modifiers1/Main1.java
@@ -0,0 +1,9 @@
+package modifiers1;
+
+public class Main1 {
+    public static void main(String args[]){
+        modifiers.A obj=new modifiers.A();
+        System.out.println(obj.x);
+    }
+    
+}
